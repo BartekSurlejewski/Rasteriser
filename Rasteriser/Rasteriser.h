c6cd6377fb7ff658a56f013e062d9e2ea8e3a34f
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2020-02-02 21:24:52 +0100

    Add more optimizations
    Change compiler settings

--- Rasteriser/Rasteriser.h
+++ Rasteriser/Rasteriser.h
@@ -16,23 +16,16 @@ public:
 protected:
 	Vector3f computeScreenCoordinates(const Vector3f& vertex) const;
 	Vector3f getBarycentricCoords(const Vector3f& vertex0, const Vector3f& vertex1, const Vector3f& vertex2,
-	                              const unsigned int& pixelX, const unsigned int& pixelY) const;
+		const unsigned int& pixelX, const unsigned int& pixelY) const;
 
-	Scene* scene;
-	std::shared_ptr<Image> image;
 	unsigned int imageWidth;
 	unsigned int imageHeight;
+	Scene* scene;
+	std::shared_ptr<Image> image;
 
 private:
 	float edgeFunction(const Vector3f& a, const Vector3f& b, const Vector2<unsigned int>& c) const;
 	float edgeFunction(const Vector3f& a, const Vector3f& b, const Vector3f& c) const;
-
-	float dx12;
-	float dx23;
-	float dx31;
-	float dy12;
-	float dy23;
-	float dy31;
 
 	float** depthBuffer;
 };
